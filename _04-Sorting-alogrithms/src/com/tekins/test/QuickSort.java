8e4016c673fc4d6ffe12b32a8d19d62ed00f9a14
Niket-mishra <niketmishraofficial@gmail.com>
2024-02-02 15:05:47 +0530

    Inital Commit

---
+++ _04-Sorting-alogrithms/src/com/tekins/test/QuickSort.java
@@ -0,0 +1,51 @@
+package com.tekins.test;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] array = {3, 6, 8, 10, 11, 2, 1};
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        quickSort(array, 0, array.length - 1);
+
+        System.out.println("\nSorted array:");
+        printArray(array);
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
