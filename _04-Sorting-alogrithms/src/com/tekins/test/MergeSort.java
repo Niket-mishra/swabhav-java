d9f0c7df77e796d00f8aa4f4f892aace30ed4050
Niket-mishra <niketmishraofficial@gmail.com>
2024-02-02 15:15:59 +0530

    Updated

--- _04-Sorting-alogrithms/src/com/tekins/test/MergeSort.java
+++ _04-Sorting-alogrithms/src/com/tekins/test/MergeSort.java
@@ -7,20 +7,15 @@ public class MergeSort {
         int n1 = m - l + 1;
         int n2 = r - m;
 
-        // Create temp arrays
         int[] L = new int[n1];
         int[] R = new int[n2];
 
-        // Copy data to temp arrays
         System.arraycopy(arr, l, L, 0, n1);
         for (int j = 0; j < n2; ++j)
             R[j] = arr[m + 1 + j];
 
-        // Merge the temp arrays
-        // Initial indexes of first and second sub arrays
         int i = 0, j = 0;
 
-        // Initial index of merged sub array
         int k = l;
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
@@ -33,14 +28,12 @@ public class MergeSort {
             k++;
         }
 
-        // Copy remaining elements of L[] if any
         while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
 
-        // Copy remaining elements of R[] if any
         while (j < n2) {
             arr[k] = R[j];
             j++;
@@ -48,23 +41,18 @@ public class MergeSort {
         }
     }
 
-    // Main function that sorts arr[l..r] using
-    // merge()
     void sort(int[] arr, int l, int r) {
         if (l < r) {
-            // Find the middle point
+
             int m = (l + r) / 2;
 
-            // Sort first and second halves
             sort(arr, l, m);
             sort(arr, m + 1, r);
 
-            // Merge the sorted halves
             merge(arr, l, m, r);
         }
     }
 
-    // A utility function to print array of size n
     static void printArray(int[] arr) {
         for (int j : arr) System.out.print(j + " ");
         System.out.println();
@@ -77,7 +65,6 @@ public class MergeSort {
         System.out.println("Given Array");
         printArray(arr);
 
-        // Calling of Merge Sort
         MergeSort ob = new MergeSort();
         ob.sort(arr, 0, arr.length - 1);
 
