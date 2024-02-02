85afe0c5cd00d7a53b6019b01b9c0a5c50a48aed
Niket-mishra <niketmishraofficial@gmail.com>
2024-02-02 13:38:45 +0530

    Update

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Human obj = new Human();
-        obj.setAge (20);
-        obj.setName ("Niket");
+        obj.setAge(20);
+        obj.setName("Niket");
 
-        System.out.println(obj.getAge()+" : "+obj.getName());
+        System.out.println(obj.getAge() + " : " + obj.getName());
 
         Human obj2 = new Human();
-        obj2.setAge (21);
-        obj2.setName ("Aniket");
+        obj2.setAge(21);
+        obj2.setName("Aniket");
 
-        System.out.println(obj2.getAge()+" : "+obj2.getName());
+        System.out.println(obj2.getAge() + " : " + obj2.getName());
     }
 }
