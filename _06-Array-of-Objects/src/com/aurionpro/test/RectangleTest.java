c0b738f0755c25617d8ae545b85bc2a44df7f6c1
Niket-mishra <niketmishraofficial@gmail.com>
2024-02-07 20:59:24 +0530

    Upate

--- _06-Array-of-Objects/src/com/aurionpro/test/RectangleTest.java
+++ _06-Array-of-Objects/src/com/aurionpro/test/RectangleTest.java
@@ -1,2 +1,21 @@
-package com.aurionpro.test;public class RectangleTest {
+package com.aurionpro.test;
+
+import com.aurionpro.model.Rectangle;
+
+import java.util.Scanner;
+
+public class RectangleTest {
+    public static void main(String[] args) {
+        Rectangle[] rectangles = new Rectangle[2];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < rectangles.length; i++) {
+            rectangles[i] = new Rectangle();
+            System.out.println("Enter length :");
+            rectangles[i].setLength(sc.nextInt());
+            System.out.println("Enter breadth :");
+            rectangles[i].setBreadth(sc.nextInt());
+            System.out.println("Area :" + rectangles[i].calculateArea());
+        }
+    }
 }
