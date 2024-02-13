852fef0d38924c63683366bf596edfc951e24c76
Niket-mishra <niketmishraofficial@gmail.com>
2024-02-13 15:05:08 +0530

    Final Commit

--- _09-Static-Variable/src/com/aurionpro/model/Student.java
+++ _09-Static-Variable/src/com/aurionpro/model/Student.java
@@ -1,2 +1,60 @@
-package com.aurionpro.model;public class Student {
+package com.aurionpro.model;
+
+public class Student {
+    private int rollNumber;
+    private String name;
+    private String email;
+    private static String university;
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public static String getUniversity() {
+        return university;
+    }
+
+    public static void setUniversity(String university) {
+        Student.university = university;
+    }
+
+    public Student(int rollNumber, String name, String email) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.email = email;
+
+    }
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNumber=" + rollNumber +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", university='" + university + '\'' +
+                '}';
+    }
 }
--- _09-Static-Variable/src/com/aurionpro/test/StudentTest.java
+++ _09-Static-Variable/src/com/aurionpro/test/StudentTest.java
@@ -1,2 +1,17 @@
-package com.aurionpro.test;public class StudentTest {
+package com.aurionpro.test;
+
+import com.aurionpro.model.Student;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student s1 =new Student(1,"Niket","niketmishraofficial@Gmail.com");
+
+        Student.setUniversity("Mumbai University");
+        Student s2 = new Student();
+        s2.setName("Niket");
+        s2.setRollNumber(2);
+        s2.setEmail("mefkdmcke@gmail.com");
+        System.out.println(s1);
+        System.out.println(s2);
+    }
 }
