206d359f04fdf1c5ab188d52f68f69341de18140
Niket-mishra <niketmishraofficial@gmail.com>
2024-02-13 15:03:37 +0530

    Final Commit of this Project

--- _11-Method-overloading/src/com/aurionpro/model/AddDemo.java
+++ _11-Method-overloading/src/com/aurionpro/model/AddDemo.java
@@ -1,2 +1,19 @@
-package com.aurionpro.model;public class AddDemo {
+package com.aurionpro.model;
+
+public class AddDemo {
+    public int add(int num1, int num2) {
+        System.out.println("Method with two Int");
+        return num1 + num2;
+    }
+
+    public double add(double num1, double num2) {
+        System.out.println("Method with two double");
+        return num1 + num2;
+    }
+
+    public double add(int num1, double num2, double num3) {
+        System.out.println("Method with one Int and two double");
+
+        return num1 + num2 + num3;
+    }
 }
--- _11-Method-overloading/src/com/aurionpro/test/AddTest.java
+++ _11-Method-overloading/src/com/aurionpro/test/AddTest.java
@@ -1,2 +1,14 @@
-package com.aurionpro.test;public class AddTest {
+package com.aurionpro.test;
+
+import com.aurionpro.model.AddDemo;
+
+public class AddTest {
+
+    public static void main(String[] args) {
+        AddDemo obj = new AddDemo();
+        System.out.println(obj.add(10, 20));
+        System.out.println(obj.add(10.5, 11.5));
+        System.out.println(obj.add(10, 20.5, 30.05));
+
+    }
 }
