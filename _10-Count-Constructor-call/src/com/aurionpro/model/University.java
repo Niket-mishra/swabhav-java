3b72aa39041abe31d07521c5055db59194f6ed72
Niket-mishra <niketmishraofficial@gmail.com>
2024-02-13 15:04:19 +0530

    Final Commit

--- _10-Count-Constructor-call/src/com/aurionpro/model/University.java
+++ _10-Count-Constructor-call/src/com/aurionpro/model/University.java
@@ -1,2 +1,44 @@
-package com.aurionpro.model;public class University {
+package com.aurionpro.model;
+
+
+
+public class University {
+    private String rollNumber;
+    private String name;
+    private String email;
+    public static int count = 1001;
+
+    public University() {
+        this.rollNumber=generateRollNumber();
+        count++;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    private static String generateRollNumber() {
+
+        return "Stud"+count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- _10-Count-Constructor-call/src/com/aurionpro/test/UniversityTest.java
+++ _10-Count-Constructor-call/src/com/aurionpro/test/UniversityTest.java
@@ -1,2 +1,18 @@
-package com.aurionpro.test;public class UniversityTest {
+package com.aurionpro.test;
+
+import com.aurionpro.model.University;
+
+public class UniversityTest {
+
+    public static void main(String[] args) {
+
+        University s1 = new University();
+        University s2= new University();
+        University s3 = new University();
+
+        System.out.println(s1.getRollNumber());
+        System.out.println(s2.getRollNumber());
+        System.out.println(s3.getRollNumber());
+
+    }
 }
