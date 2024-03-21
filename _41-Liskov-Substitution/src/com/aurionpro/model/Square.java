f4f18093190fc867f2fd94222baa4ffacc315d2e
Niket-mishra <niketmishraofficial@gmail.com>
2024-03-21 14:52:39 +0530

    Final Commit

--- _41-Liskov-Substitution/src/com/aurionpro/model/Square.java
+++ _41-Liskov-Substitution/src/com/aurionpro/model/Square.java
@@ -1,2 +1,12 @@
-package com.aurionpro.model;public class Square {
+package com.aurionpro.model;
+
+public class Square extends Rectangle{
+    public Square(int side) {
+        super(side, side);
+    }
+    public void setHeight(int height){
+        super.setHeight(height);
+        super.setWidth(height);
+    }
+
 }
--- _41-Liskov-Substitution/src/com/aurionpro/test/ShapeTest.java
+++ _41-Liskov-Substitution/src/com/aurionpro/test/ShapeTest.java
@@ -1,2 +1,29 @@
-package com.aurionpro.test;public class ShapeTest {
+package com.aurionpro.test;
+
+import com.aurionpro.model.Rectangle;
+import com.aurionpro.model.Square;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(10, 20);
+        Rectangle square = new Square(10);
+        System.out.println(isRectangle(rectangle));
+        System.out.println(isRectangle(square));
+
+    }
+    public static boolean isRectangle(Rectangle obj) {
+        int height = obj.getHeight();
+        int prev = obj.getWidth();
+        obj.setHeight(100);
+        boolean b = false;
+        if (height != obj.getHeight()) {
+            int curr = obj.getWidth();
+            b = prev == curr;
+        }
+
+
+        return b;
+    }
+
 }
+
